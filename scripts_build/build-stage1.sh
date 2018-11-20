587c96ee23c802b02b355c098432a3ca27970890
RBUSTAMANTEG <rbustamanteg@localhost.localdomain>
2018-11-20 15:48:45 -0500

    Added configuration scripts. Pending adjustments to exec_when_ready scripts and  command

--- scripts_build/build-stage1.sh
+++ scripts_build/build-stage1.sh
@@ -32,18 +32,18 @@ cat <<EOF > "${HOME}/.terraformrc"
 plugin_cache_dir = "${HOME}/.terraform.d/plugin-cache"
 EOF
 
-# INSTALLER 
-cat <<EOF >>"${HOME}/.bash_profile"
-export GOBIN='/opt/app-root/bin'
-export OPENSHIFT_INSTALL_BASE_DOMAIN=tt.testing
-export OPENSHIFT_INSTALL_CLUSTER_NAME=test1
-export OPENSHIFT_INSTALL_EMAIL_ADDRESS=admin@openshiftdemo.org
-export OPENSHIFT_INSTALL_PASSWORD=verysecure
-export OPENSHIFT_INSTALL_PLATFORM=libvirt
-export OPENSHIFT_INSTALL_PULL_SECRET='{"auths": {"quay.io": {"auth": "Y29yZW9zK3RlYzJfaWZidWdsandoYTNkaW4yYmlqY2MybjJlaTpLMVNEQjNVWTlHMUZQUFpKTFg5VTFOWlJIQjRURjA1WllEOTZBMjdSWTRYRVcwSVVKVkxWRFZFSDFDSlZNQlNS", "email": "" }}}'
-export OPENSHIFT_INSTALL_SSH_PUB_KEY="$(cat /root/.ssh/id_rsa.pub)"
-export OPENSHIFT_INSTALL_LIBVIRT_URI=qemu+tcp://192.168.122.1/system
-#export OPENSHIFT_INSTALL_LIBVIRT_IMAGE=http://aos-ostree.rhev-ci-vms.eng.rdu2.redhat.com/rhcos/images/cloud/latest/rhcos-qemu.qcow2.gz
-export OPENSHIFT_INSTALL_LIBVIRT_IMAGE=file:///opt/app-root/src/qemu-img/rhcos-qemu.qcow2 
-export KUBECONFIG=/opt/app-root/src/github.com/openshift/installer/auth/kubeconfig
-EOF
+## INSTALLER 
+#cat <<EOF >>"${HOME}/.bash_profile"
+#export GOBIN='/opt/app-root/bin'
+#export OPENSHIFT_INSTALL_BASE_DOMAIN=tt.testing
+#export OPENSHIFT_INSTALL_CLUSTER_NAME=test1
+#export OPENSHIFT_INSTALL_EMAIL_ADDRESS=admin@openshiftdemo.org
+#export OPENSHIFT_INSTALL_PASSWORD=verysecure
+#export OPENSHIFT_INSTALL_PLATFORM=libvirt
+#export OPENSHIFT_INSTALL_PULL_SECRET='{"auths": {"quay.io": {"auth": "Y29yZW9zK3RlYzJfaWZidWdsandoYTNkaW4yYmlqY2MybjJlaTpLMVNEQjNVWTlHMUZQUFpKTFg5VTFOWlJIQjRURjA1WllEOTZBMjdSWTRYRVcwSVVKVkxWRFZFSDFDSlZNQlNS", "email": "" }}}'
+#export OPENSHIFT_INSTALL_SSH_PUB_KEY="$(cat /root/.ssh/id_rsa.pub)"
+#export OPENSHIFT_INSTALL_LIBVIRT_URI=qemu+tcp://192.168.122.1/system
+#export OPENSHIFT_INSTALL_LIBVIRT_IMAGE=file:///opt/app-root/src/qemu-img/rhcos-qemu.qcow2 
+#export KUBECONFIG=/opt/app-root/src/github.com/openshift/installer/auth/kubeconfig
+#export PATH="${PATH}:${GOBIN}"
+#EOF
