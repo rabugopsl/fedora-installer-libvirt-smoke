81853b9793bd03523b8f7093c17c4bc6520fc3a2
RBUSTAMANTEG <rbustamanteg@localhost.localdomain>
2018-11-22 15:11:47 -0500

    Added  and  to the container build process

--- scripts_build/build-stage2.sh
+++ scripts_build/build-stage2.sh
@@ -7,4 +7,9 @@ export REVISION=1
 mkdir /opt/app-root/bin
 mkdir /opt/app-root/pkg
 
+# Terraform plugin
 GOBIN=~/.terraform.d/plugins go get github.com/dmacvicar/terraform-provider-libvirt
+
+# GINKGO
+go get -u github.com/onsi/ginkgo/ginkgo
+go get -u github.com/onsi/gomega/...
